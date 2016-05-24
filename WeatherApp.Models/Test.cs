4897f10a4d2dcfd8be402223911456ad5735a7d4
Sonny <sk222sw@student.lnu.se>
2016-05-24 19:18:12 +0200

    new project for models

---
+++ WeatherApp.Models/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeatherApp.Models
+{
+    public class Test
+    {
+        public string Greet
+        {
+            get
+            {
+                return "Greetings from another project.";
+            }
+        }
+    }
+}
--- WeatherApp/Controllers/HomeController.cs
+++ WeatherApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WeatherApp.Models;
 
 namespace WeatherApp.Controllers
 {
@@ -10,7 +11,8 @@ namespace WeatherApp.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var test = new Test();
+            return View(test);
         }
     }
 }
